d97b9261b7b75048b95e9b56a895bf2498fc4216
NonameNoname123 <zhenya.grigorieva116@yandex.ru>
2018-11-11 19:39:46 +0300

    Добавьте файлы проекта.

---
+++ WebPolyclinic/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebPolyclinic.MvcApplication" Language="C#" %>
---
+++ WebPolyclinic/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebPolyclinic.Startup))]
+namespace WebPolyclinic
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
